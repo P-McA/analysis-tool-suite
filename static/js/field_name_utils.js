acdd556eec8f55116fe0204c2dd3f43d51e39332
P-McA <Connorhouse1994.>
2025-05-26 21:24:22 +0100

    fixComparisonTool Updates

---
+++ static/js/field_name_utils.js
@@ -0,0 +1,9 @@
+// Function to get FIX field name mappings
+import { fixFieldMap } from '../../templates/fix_fields_global.js';
+
+const getFieldNameMap = () => {
+  // Return the imported field mappings
+  return fixFieldMap;
+};
+
+export default getFieldNameMap;
